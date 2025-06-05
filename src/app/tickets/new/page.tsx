44a359f8b3976e861394f3c8ad1836b25cfd5656
Shane <shanep42@gmail.com>
2025-06-05 14:36:47 -0500

    Ticket detail page template added

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,52 +1,9 @@
-'use client'
-
-import { useActionState } from "react";
-import { createTicket } from "@/actions/ticket.actions";
+import NewTicketForm from "./ticket-form";
 
 const NewTicketPage = () => {
-    const [state, formAction] = useActionState(createTicket, {
-        success: false,
-         message:''
-        })
-
-    return (
-    <div className="min-h-screen bg-blue-50 flex items-center justify-center px-4">
-        <div className="w-full max-w-mdbg-white shadow-md rounded-lg p-8 border border-gray-200">
-            <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">
-                Submit a Support Ticket
-            </h1>
-            <form action={formAction}className="space-y-4 text-gray-700">
-                <input 
-                    className="w-full border border-gray-200 p-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    type="text"
-                    name="subject"
-                    placeholder="Subject"    
-                    />
-                <textarea 
-                    className="w-full border border-gray-200 p-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    name="description" 
-                    placeholder="Decribe your issue"
-                    rows={4}
-                />
-                <select 
-                    className="w-full border border-gray-200 p-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    name="priority"
-                    defaultValue="Low"
-                >
-                    <option value="Low">Low Priority</option>
-                    <option value="Medium">Medium Priority</option>
-                    <option value="High">High Priority</option>
-                </select>
-                <button 
-                    className="w-full bg-blue-600 text-white p-3 rounded hover:bg-blue-700 transition disabled:opacity-50"
-                    type="submit"
-                    >
-                        Submit
-                    </button>
-            </form>
-        </div>
-    </div>
-    )
-};
-
+    return ( <div className="min-h-screen bg-blue-50 flex items-center justify-center px-4">
+        <NewTicketForm/>
+    </div> );
+}
+ 
 export default NewTicketPage;
